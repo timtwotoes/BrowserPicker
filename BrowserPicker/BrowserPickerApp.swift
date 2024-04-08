1df0b0961e3bab23ceb36b780eedb7eb08654714
Tim Wolff <timtwotoes@me.com>
2024-04-08 19:20:36 +0200

    Initial Commit

---
+++ BrowserPicker/BrowserPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserPickerApp.swift
+//  BrowserPicker
+//
+//  Created by Tim on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrowserPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
