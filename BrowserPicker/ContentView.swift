9d09e10a6ece3e52605af7ebc21f1ca4d45c1658
Tim Wolff <timtwotoes@me.com>
2024-04-13 01:39:00 +0200

    Complete application functionality

--- BrowserPicker/ContentView.swift
+++ BrowserPicker/ContentView.swift
@@ -1,27 +1,41 @@
 import SwiftUI
-import BrowserAvailability
 
 struct ContentView: View {
     @StateObject private var browserDetector = BrowserDetector()
+    @FocusState private var firstResponder: Bool
     @State private var browserDestination = ""
     
     var body: some View {
         VStack {
-            Picker("Browser", selection: $browserDetector.selectedBrowser) {
-                ForEach(browserDetector.availableBrowsers) { entry in
-                    Text(entry.localizedName).tag(entry.url)
-                }
-            }.labelsHidden()
-            
             HStack {
+                Picker("Browser", selection: $browserDetector.selectedBrowser) {
+                    ForEach(browserDetector.availableBrowsers) { entry in
+                        Text(entry.localizedName).tag(entry.url)
+                    }
+                }
+                .labelsHidden()
+                .fixedSize()
+
                 TextField("Enter URL", text: $browserDestination)
+                    .frame(idealWidth: 200)
+                    .focused($firstResponder)
                 Button("Go") {
                     NSWorkspace.shared.open([URL(string: browserDestination)!], withApplicationAt: browserDetector.selectedBrowser, configuration: NSWorkspace.OpenConfiguration())
-                }.keyboardShortcut(.defaultAction)
+                }
+                .keyboardShortcut(.defaultAction)
+                .disabled(isInvalidURL(string: browserDestination))
+                
             }
         }
-        .frame(minWidth: 250, maxWidth: 250)
+        .onAppear {
+            firstResponder = true
+        }
         .padding()
+        .fixedSize()
+    }
+    
+    func isInvalidURL(string: String) -> Bool {
+        return (string.hasPrefix("http:") || string.hasPrefix("https:")) == false
     }
 }
 
